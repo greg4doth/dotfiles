f3a9451c4ca63817a2fd775f4f32940cd6b030a6
nekochemist <nekochemist@gmail.com>
2025-05-08 22:31:11 +0500

    изменено:      install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ for dir in wlogout waybar vesktop fastfetch fuzzel hypr kitty nvim obsidian ; do
 
     rsync -rp "$thdir/$dir" "$HOME/.config/"
 done
+mkdir /$HOME/scripts
 for scr in scripts ; do 
-    rsync -rp "$thdir/$scr" "$HOME/d/scripts/"
+    rsync -rp "$thdir/$scr" "$HOME/scripts/"
 done
